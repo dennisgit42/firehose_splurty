3635d9f72967583f493b11168965da2329807caf
Dennis Ouyang <den2net@yahoo.com>
2017-05-30 03:01:37 +0000

    Add info about pizza being awesome

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,12 +3,13 @@ class QuotesController < ApplicationController
     @quote = Quote.order("RANDOM()").first
   end
 
-
-
   def create
     @quote = Quote.create(quote_params)
     if @quote.invalid?
-      flash[:error] = "<strong>Could not save</strong> the data you entered is invalid."
+      flash[:error] = "<strong>Could not save</strong> the data you entered is invalid. Please enter minimum of 3 characters for both saying and author
+      , and maximum of 140 characters for saying and 50 characters for author."
+    else 
+      flash[:notice] = "The quote you entered has been saved!"
     end
     redirect_to root_path
   end
